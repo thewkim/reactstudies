4d369e600190fc2dfedd8081207e7bbb1afe40f2
williamj17tech <william.kim@j17tech.com.br>
2024-08-22 00:08:37 -0300

    react + vite + docker boilerplate inicial

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Isso permite que o Vite escute em todas as interfaces
+    port: 5173,
+  },
+})
+
